fe48922a178d67fcbdbd164c80bbc05bc1fe1309
1406MJM <3330kys@dsm.hs.kr>
2022-03-14 18:47:22 +0900

    [EDIT] index.js 파일에도 라우터 및 mysql 연결 설정

--- expressApp/routes/index.js
+++ expressApp/routes/index.js
@@ -1,5 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const mysql = require('mysql')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
