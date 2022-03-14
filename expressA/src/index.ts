fcef32a74c18fd7e34519175779a14518526924b
1406MJM <3330kys@dsm.hs.kr>
2022-03-15 08:44:33 +0900

    [ADD] 새로운 폴더 생성 및 typeorm 연결

---
+++ expressA/src/index.ts
@@ -0,0 +1,7 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+
+createConnection().then(async connection => {
+    console.log('start')
+}).catch(error => console.log(error));
