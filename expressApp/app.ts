8883a5482d3c4b009f1de6416a6707d31ce030fb
1406MJM <3330kys@dsm.hs.kr>
2022-03-15 08:45:00 +0900

    [EDIT] 기존 파일들 연결

--- expressApp/app.ts
+++ expressApp/app.ts
@@ -1,13 +1,9 @@
-import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
 import bodyParser from 'body-parser';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 import { createConnection } from 'typeorm';
-import connectionOptions from './config/ormconfig';
 
 const app = express();
 
@@ -15,35 +11,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')));
 
-createConnection(connectionOptions).then(async connection => {
-  app.listen(3000)
-}).catch(err => console.log(err))
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
