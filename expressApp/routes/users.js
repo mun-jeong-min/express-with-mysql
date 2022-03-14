e689c9eeb3257f16b76af941baa799436a105a6b
1406MJM <3330kys@dsm.hs.kr>
2022-03-14 17:25:33 +0900

    [ADD] mysql 연결 및 테이블 생성 쿼리 추가

--- expressApp/routes/users.js
+++ expressApp/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const mysql = require('mysql')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
+connection.connect();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/s', async(res,req) => {
+  connection.query('CREATE TABLE tsa(age int(50))', async(err,results,fields) => {
+    if(err) {
+      console.log(err)
+    }
+    console.log(results)
+    console.log(fields)
+  })
+})
 module.exports = router;
