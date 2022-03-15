b710e6d89ee0a3531542a20dcd4076aa7e058055
1406MJM <3330kys@dsm.hs.kr>
2022-03-15 19:30:50 +0900

    [EDIT] 비밀번호 hash 및 주석처리

--- expressA/src/user/user.ts
+++ expressA/src/user/user.ts
@@ -1,17 +1,40 @@
 import * as express from 'express';
+import {hash, compare} from 'bcrypt'
 import { userDto } from './dto/user.dto';
 import { User } from './entity/user.entity';
+import * as bcrypt from 'bcrypt'
 const router = express.Router();
+import * as dotenv from 'dotenv'
+dotenv.config();
 
 router.post('/signup', async(req,res) => {
     try {
-    const { role, name, password }:userDto = req.body;
+    let { role, name, password }:userDto = req.body;
 
-    const user = await User.create({ name, password, role })
+    const hashPassword = bcrypt.hashSync(password, process.env.salt)
+    password = hashPassword;
+
+    const user = await User.create({ role, name, password})
     await user.save()
+
     return res.json({ success:true })
     } catch (e) {
         return res.json({ success:false, e })
     }
 })
+
+router.post('/signin', async(res,req) => {
+    
+})
+/*
+router.get('/find', async(req,res) => {
+    try {
+      const users = await User.find()
+      return res.send({ users })   
+    } catch (error) {
+        return res.json({ success:"404", error })
+    }
+})*/
+
+
 export default router
