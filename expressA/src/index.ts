260dff96ea83e143870bf5ee37994dad182324c7
1406MJM <3330kys@dsm.hs.kr>
2022-03-15 17:21:26 +0900

    [EDIT] router 설정, 앱연결

--- expressA/src/index.ts
+++ expressA/src/index.ts
@@ -1,7 +1,20 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./user/entity/User";
+import {User} from "./user/entity/user.entity";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+const app = express()
+
+import router from './user/user'
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.use('/user', router);
 
 createConnection().then(async connection => {
-    console.log('start')
+    console.log('connect..')
 }).catch(error => console.log(error));
+
+app.listen(3000)
+export default app
