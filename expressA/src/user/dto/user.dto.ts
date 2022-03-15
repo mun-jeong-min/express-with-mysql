80fa3958b03bada31818606e332ab796e8a895d7
1406MJM <3330kys@dsm.hs.kr>
2022-03-15 18:54:15 +0900

    [EDIT] dto 처리, 회원가입 로직에 에러처리

--- expressA/src/user/dto/user.dto.ts
+++ expressA/src/user/dto/user.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from "class-validator";
 export class userDto {
     @IsString()
     @IsNotEmpty()
-    id:string;
+    role:string;
 
     @IsString()
     @IsNotEmpty()
--- expressA/src/user/user.ts
+++ expressA/src/user/user.ts
@@ -1,12 +1,17 @@
 import * as express from 'express';
+import { userDto } from './dto/user.dto';
 import { User } from './entity/user.entity';
 const router = express.Router();
 
 router.post('/signup', async(req,res) => {
-    const { role, name, password } = req.body;
+    try {
+    const { role, name, password }:userDto = req.body;
 
     const user = await User.create({ name, password, role })
     await user.save()
+    return res.json({ success:true })
+    } catch (e) {
+        return res.json({ success:false, e })
+    }
 })
-
 export default router
