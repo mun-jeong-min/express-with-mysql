a1d7eccb6c45c11ae4f6d2d70ee07301bd45824f
1406MJM <3330kys@dsm.hs.kr>
2022-03-15 09:22:50 +0900

    [EDIT] 모듈 별로 entity 나누고 경로 수정

--- expressA/src/index.ts
+++ expressA/src/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import {User} from "./user/entity/User";
 
 createConnection().then(async connection => {
     console.log('start')
