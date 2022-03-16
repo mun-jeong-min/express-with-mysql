27357bdd2f59e41d9ac79ec6de2bcf78f3e48a71
1406MJM <3330kys@dsm.hs.kr>
2022-03-16 17:13:00 +0900

    [EDIT] login 라우터 추가, 토큰 발급

--- expressA/src/user/user.ts
+++ expressA/src/user/user.ts
@@ -1,11 +1,13 @@
 import * as express from 'express';
 import {hash, compare} from 'bcrypt'
-import { userDto } from './dto/user.dto';
+import { userDto } from './dto/signup.dto';
 import { User } from './entity/user.entity';
 import * as bcrypt from 'bcrypt'
 const router = express.Router();
 import * as dotenv from 'dotenv'
 import { getRepository } from 'typeorm';
+import { loginDto } from './dto/login.dto';
+import * as jwt from 'jsonwebtoken'
 dotenv.config();
 
 router.post('/signup', async(req: express.Request, res: express.Response) => {
@@ -26,6 +28,30 @@ router.post('/signup', async(req: express.Request, res: express.Response) => {
         return res.status(409).send("password already in use")  
     }
     res.status(201).send("user created")
+})
+
+router.post('/signin', async(req:express.Request, res:express.Response) => {
+    let {name, password}:loginDto = req.body;
+
+    const userRepository = getRepository(User);
+    let user: User;
+    try {
+        user = await userRepository.findOneOrFail({ where: {name}})
+    } catch (e) {
+        res.status(404).send(" 일치하는 유저가 없음");
+    }
+
+    if(!bcrypt.compareSync(password, user.password)){
+        res.status(401).send();
+        return;
+    }
+    const token = jwt.sign(
+        {userId: user.id, userName: name},
+        process.env.JWT_SECRET,
+        {expiresIn:'1h'}
+    )
+
+    res.status(200).send(`로그인 성공 ${token}`)
 })
 
 export default router
