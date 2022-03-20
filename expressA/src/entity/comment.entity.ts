c57e6626cfb1da0b7749f35365f7cfcc12b18254
1406MJM <3330kys@dsm.hs.kr>
2022-03-20 22:20:58 +0900

    [ADD] comment 엔티티 추가

---
+++ expressA/src/entity/comment.entity.ts
@@ -0,0 +1,15 @@
+import { type } from "os";
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Board } from "./board.entity";
+
+@Entity()
+export class Comment extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    script: string;
+
+    @ManyToOne(type => Board, board=>board.comment, {eager:true})
+    board:Board;
+}
