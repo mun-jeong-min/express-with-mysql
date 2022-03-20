c8d62b33e0bc035f94a0ba1f329d7ec3e8631ee0
1406MJM <3330kys@dsm.hs.kr>
2022-03-21 08:44:58 +0900

    [EDIT] comment 테이블에 user id 넣는 로직

--- expressA/src/router/comment.ts
+++ expressA/src/router/comment.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import { getRepository } from 'typeorm';
 import { commentDto } from '../entity/comment/dto/comment.dto';
 import { Comment } from '../entity/comment/comment.entity';
+import { User } from '../entity/user/user.entity';
 
 const router = express.Router();
 
@@ -9,9 +10,15 @@ router.post('/create', async(req:express.Request, res:express.Response) => {
     let {script}:commentDto = req.body;
 
     const commentRepository = await getRepository(Comment);
+    const userRepository = await getRepository(User);
+
+    const id = await res.locals.jwtPayload.userId;
+    const user = await userRepository.findOneOrFail({where: {id:id}});
+
     let comment = new Comment();
 
     comment.script = script;
+    comment.user = user;
 
     try {
         await commentRepository.save(comment);
@@ -21,3 +28,5 @@ router.post('/create', async(req:express.Request, res:express.Response) => {
     }
     res.status(200).send()
 })
+
+export default router
