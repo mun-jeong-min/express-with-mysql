8abef7f1f6a9ba5c7f5c69e12d9a4c2a4cc158e2
1406MJM <3330kys@dsm.hs.kr>
2022-03-22 09:40:06 +0900

    [ADD] 댓글을 모두 찾아주는 로직 작성

--- expressA/src/router/comment.ts
+++ expressA/src/router/comment.ts
@@ -18,7 +18,7 @@ router.post('/create/:id', async(req:express.Request, res:express.Response) => {
     const id = await res.locals.jwtPayload.userId;
     
     const user = await userRepository.findOneOrFail({where: {id:id}});
-    const board = await boardRepository.findOneOrFail({where: {id: idP}})
+    const board = await boardRepository.findOneOrFail({where: {id: idP}});
 
     let comment = new Comment();
 
@@ -33,6 +33,17 @@ router.post('/create/:id', async(req:express.Request, res:express.Response) => {
         return;
     }
     return res.status(200).send("댓글 작성")
+})
+
+router.get('/find', async(req:express.Request, res: express.Response) => {
+    const commentRepository = getRepository(Comment);
+    
+    try {
+     const data = await commentRepository.find();
+     res.status(200).send(data);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 })
 
 export default router
