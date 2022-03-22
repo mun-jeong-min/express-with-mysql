9f7c0b1cb6bc72b873d708da33c1bbe182f601c3
1406MJM <3330kys@dsm.hs.kr>
2022-03-22 19:35:40 +0900

    [ADD] 댓글 삭제 로직 추가

--- expressA/src/router/comment.ts
+++ expressA/src/router/comment.ts
@@ -64,4 +64,19 @@ router.put('/update/:id', async(req:express.Request, res:express.Response) => {
     }
     res.status(200).send('업데이트 완료')
 })
+
+router.delete('/delete/:id', async(req:express.Request, res:express.Response) => {
+    const commentRepository = getRepository(Comment);
+    const id = req.params.id;
+
+    const comment = await commentRepository.findOne({where: {id:id}});
+
+    try {
+        await commentRepository.delete(comment);
+    } catch (e) {
+        res.status(400).send()
+        return;
+    }
+    res.status(200).send("댓글 삭제 성공")
+})
 export default router
