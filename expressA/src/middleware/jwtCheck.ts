f622b6872f742fa1c8e66d019956d1b422de55c3
1406MJM <3330kys@dsm.hs.kr>
2022-03-23 22:00:33 +0900

    [EDIT] refresh 토큰과 구별하기 위해 수정

--- expressA/src/middleware/jwtCheck.ts
+++ expressA/src/middleware/jwtCheck.ts
@@ -1,15 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import * as jwt from 'jsonwebtoken'
+import redisClient from "../redis/redis";
 import * as dotenv from 'dotenv'
 dotenv.config();
 
 export const tokenCheck = (req:Request, res:Response, next:NextFunction) => {
-    const token = <string>req.headers['auth'];
+    const token = <string>req.headers['access'];
     let jwtPayload;
 
     try {
-        jwtPayload = <any>jwt.verify(token, process.env.JWT_SECRET)
+        jwtPayload = <any>jwt.verify(token, process.env.JWT_ACCESS_SECRET)
         res.locals.jwtPayload = jwtPayload;  // 미들웨어를 거쳐 어디에서나 쓸수 있는 변수로 만듬
+
     } catch (e) {
         res.status(400).send();
     }
@@ -17,11 +19,11 @@ export const tokenCheck = (req:Request, res:Response, next:NextFunction) => {
     let {userId, userName} = jwtPayload;
     const accessToken = jwt.sign(
         {userId, userName},
-        process.env.JWT_SECRET,
+        process.env.JWT_ACCESS_SECRET,
         {expiresIn: '1h'}
     );
 
-    res.setHeader("token", accessToken); // 키/값 을 인자로 받아 헤더에 세팅한다.
+    res.setHeader("access", accessToken); // 키/값 을 인자로 받아 헤더에 세팅한다.
 
     next();
 }
