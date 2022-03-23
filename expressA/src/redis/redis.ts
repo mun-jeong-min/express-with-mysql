bed7c0fde63c35cede0c384af56a4803d60e5dd9
1406MJM <3330kys@dsm.hs.kr>
2022-03-23 22:01:02 +0900

    [EDIT] redis 모듈 잘 받을 수 있게 수정

--- expressA/src/redis/redis.ts
+++ expressA/src/redis/redis.ts
@@ -2,13 +2,13 @@ import * as redis from 'redis';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const info = {
-    host: process.env.REDIS_HOST,
-    port: +process.env.REDIS_PORT,
-    db: +process.env.REDIS_DB,
+const redisInfo = {
+    host: '127.0.0.1',
+    port: process.env.REDIS_PORT,
+    db: process.env.REDIS_DB,
     password: process.env.REDIS_PASSWORD
 }
 
-const redisClient = redis.createClient(info)
+const redisClient = redis.createClient(redisInfo)
 
-module.exports = redisClient;
+export default redisClient;
