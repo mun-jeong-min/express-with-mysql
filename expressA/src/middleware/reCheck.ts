7a21d5805d1c2699c043a675db4fb0cbb2fc4f08
1406MJM <3330kys@dsm.hs.kr>
2022-03-23 22:02:04 +0900

    [ADD] refreshtoken 검증 로직 추가

---
+++ expressA/src/middleware/reCheck.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { promisify } from "util";
+import * as jwt from 'jsonwebtoken'
+import redisClient from "../redis/redis";
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+export const refreshCheck = async(req:Request, res:Response, next:NextFunction, userId) => {
+    const token = <string>req.headers['refresh']
+
+    const getAsync = await promisify(redisClient.get).bind(redisClient)
+
+    const data = await getAsync(userId);
+    if(token === data){
+        try {
+            jwt.verify(token,process.env.JWT_REFRESH_SECRET)
+        } catch (e) {
+            res.status(400).send();
+            return false
+        }       
+    }
+
+    const refreshToken = jwt.sign(
+        {},
+        process.env.JWT_REFRESH_SECRET,
+        {expiresIn:'12d'}
+    )
+
+    res.setHeader("refresh", refreshToken);
+
+    next();
+}
