1eaa22cbf1d5b9673f0379030921a93f58279e53
1406MJM <3330kys@dsm.hs.kr>
2022-03-27 22:46:24 +0900

    [ADD] refresh 토큰 검증후 재발급을 위해 라우터 설정

---
+++ expressA/src/router/refresh.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import * as jwt from 'jsonwebtoken'
+import { promisify } from 'util';
+import redisClient from '../redis/redis';
+import router from './user';
+
+router.post('/refresh', async(req:Request, res:Response) => {
+    const reToken = <string>req.headers['refresh'];
+    // accesstoken 유효기간 체크
+    try {
+        const getA = promisify(redisClient.get).bind(redisClient)
+        const data = getA(res.locals.jwtPayload.id)
+        
+        
+    } catch (e) {
+        
+    }
+    
+    
+    // 없으면 refreshtoken 있는지 확인
+    
+    // 있으면 재발급
+})
