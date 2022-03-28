82ef13b62d84f9fb0f4a463929b6337a13163abf
1406MJM <3330kys@dsm.hs.kr>
2022-03-28 12:14:41 +0900

    [EDIT] ejs 파일 추가 및 연결

--- expressA/src/index.ts
+++ expressA/src/index.ts
@@ -8,18 +8,24 @@ import * as cors from 'cors'
 import userRouter from './router/user'
 import boardRouter from './router/board'
 import commentRouter from './router/comment'
-
 import { tokenCheck } from "./middleware/jwtCheck";
+import * as ejs from 'ejs'
 
 createConnection().then(async connection => {
     app.use(bodyParser.urlencoded({extended:true}))
     app.use(bodyParser.json())
     app.use(cors())
 
+    app.set('view engine', 'ejs')
+    app.use(express.static(__dirname + '/'))
+
     app.use('/user', userRouter);
-    app.use('/board', [tokenCheck], boardRouter); 
-    app.use('/comment', [tokenCheck], commentRouter);
+    app.use('/board',[tokenCheck], boardRouter); 
+    app.use('/comment',[tokenCheck], commentRouter);
     
+    app.get('/', async(req,res) => {
+        res.render("as")
+    })
     console.log('connect..')
     app.listen(3000)
 }).catch(error => console.log(error));
