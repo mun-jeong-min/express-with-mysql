0e91c6fd8b178b6ddfa3652f8f6dd675ce386310
1406MJM <3330kys@dsm.hs.kr>
2022-04-01 19:37:38 +0900

    [ADD] profile 업데이트 로직 추가

--- expressA/src/controller/user/profile-controller.ts
+++ expressA/src/controller/user/profile-controller.ts
@@ -1,23 +1,43 @@
 import { getRepository } from "typeorm"
+import { Profile } from "../../entity/profile/profile.entity";
 import { User } from "../../entity/user/user.entity"
 
 export const profilePrint = async(req,res) => {
     const id = res.locals.jwtPayload.userId;
-    
-    const userRepository = getRepository(User);
+    const idP = req.params.id;
 
+    const userRepository = getRepository(User);
+    const profileRepository = getRepository(Profile);
+
+    const profile = await profileRepository.findOneOrFail({where:{id:idP}})
     const user = await userRepository.findOneOrFail({where:{id:id}})
+
+    profile.name = user.name
 
     if(!user) {
         res.status(404).send()
         return;
     }
 
-    res.status(200).send(user);
+    res.status(200).send(profile);
 }
 
 export const profileUpdate = async(req,res) => {
     const id = req.params.id;
+    let {name} = req.body;
+
+    const profileRepository = getRepository(Profile)
+    const profile = await profileRepository.findOneOrFail({where:{id:id}})
+
+    if(!profile) {
+        res.status(404).send();
+    }
+
+    profile.name = name;
+
+    await profileRepository.save(profile);
+
+    res.status(200).send();
 }
 
 export default {profilePrint,profileUpdate}
